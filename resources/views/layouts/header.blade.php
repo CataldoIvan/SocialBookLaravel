d1469cb78a941d4f82348b1242b2604d1bf01f2e
CataldoIvan <cmiarg@gmail.com>
2019-12-08 23:08:24 -0300

    configuro nuevo barra nav responsive, solo se ejecuta en la pagina 'muro'

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,85 +12,170 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600,700" rel="stylesheet">
-    <link rel="stylesheet" href="/css/styleperfil.css" type="text/css">
+    <!-- <link rel="stylesheet" href="/css/styleperfil.css" type="text/css"> -->
 </head>
 
-<body id="wall">
+<body  class='container' id="wall" style="margin:0px;">
+  <div class="row">
+    <header class="fixed-top " style="color:white;  z-index: 1;">
+        <div class=" navbar navbar-expand-md bg-dark navbar-dark just d-flex justify-content-end ">
+            <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offset-lg-2 collapse navbar-collapse align-item-right justify-content-around" id="menu">
+                <div class="buscador col-md-4 col-lg-2 my-2 text-right ">
+                    <form class="text-left search " method="GET">
+                        <input name="buscador"  class="w-100" type="text" placeholder="Buscar.." >
+                    </form>
+                </div>
+                <div class="col-xs-12 col-md-2 col-lg-1 text-right   my-2  px-0">
+                    <div class=' menu-icon'>
+                        <span class='col text-white'><a href="Perfil" title="Perfil">Perfil<i class="fa fa-user"></i></a>
+                        </span>
+                    </div>  
+                </div>
+                <div class="col-xs-12 col-md-2 col-lg-1  my-2   text-right px-0">
+                    <div class=' menu-icon'>
+                        <span class='col '><a href="muro" title="Wall">Muro<i class="fa fa-database" ></i></a>
+                        </span>
+                    </div>  
+                </div>
+                <div class=" col-12 col-md-2 col-lg-2 second-icon dropdown menu-icon my-2  text-right ">
+                    <ul class="dropdown-menu " aria-labelledby="dropdownNotification">
+                        <li class="new-not">
+                            <a href="#" title="Nombre de usuario comment"><img src="img/Amigo2.jpg" alt="Imagen de perfil del usuario" class="img-circle img-user-mini"> User comments your post</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#" title="All notifications">All Notifications</a></li>
+                    </ul>
+                    <span class="col-12 dropdown-toggle  d-inline-block " role="button" id="dropdownNotification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <span class="d-inline-block">Notificaciones</span> <i class="fa fa-bell-o" aria-hidden="true"></i> <span class="badge">2</span>
+                    </span>
+                </div>
+                <div class="dropdown col-12 col-md-2 col-lg-2  my-2 text-right ">
+                        <div>
+                            <span class="dropdown-toggle" role="button" id="dropdownSettings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            <span class="">Configuracion  <i class="fa fa-cogs" ></i> </span> 
+                            </span>
+                            <ul class="dropdown-menu " aria-labelledby="dropdownSettings">
+                                <li>
+                                    <a href="#" title="Editar usuario" data-toggle="modal" data-target="#settingsmodal">
+                                        <div class="col-xs-8 ">
+                                            <i class="fa fa-wrench"></i>
+                                            <span>Editar usuario</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-    <!--Header-->
-    <header class="text-right">
-        <form class="text-left search" method="GET">
-            <input name="q" type="text" placeholder="Buscar..">
-        </form>
-        <div class="menu-icon">
-            <div class="dropdown">
-                <span class="dropdown-toggle" role="button" id="dropdownSettings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                  <span class="hidden-xs hidden-sm">Configuracion</span> <i class="fa fa-cogs" aria-hidden="true"></i>
-                </span>
-                <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownSettings">
-                    <li>
-                        <a href="#" title="Editar usuario" data-toggle="modal" data-target="#settingsmodal">
-                            <div class="col-xs-4">
-                                <i class="fa fa-wrench" aria-hidden="true"></i>
-                            </div>
-                            <div class="col-xs-8 text-left">
-                                <span>Editar usuario</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </li>
+                            </ul>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </li>
-                </ul>
+                        </div>
+                        
+                    </div>
+                
+
             </div>
-        </div>
-        <div class="second-icon dropdown menu-icon">
-            <span class="dropdown-toggle" role="button" id="dropdownNotification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-        <span class=".d-none .d-md-block .d-lg-none">Notificaciones</span> <i class="fa fa-bell-o" aria-hidden="true"></i> <span class="badge">2</span>
-            </span>
-            <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownNotification">
-                <li class="new-not">
-                    <a href="#" title="Nombre de usuario comment"><img src="img/Amigo2.jpg" alt="Imagen de perfil del usuario" class="img-circle img-user-mini"> User comments your post</a>
-                </li>
-                <li class="new-not">
-                    <a href="#" title="Nombre de usuario comment"><img src="img/user3.jpg" alt="Imagen de perfil del usuario" class="img-circle img-user-mini"> User comments your post</a>
-                </li>
-                <li>
-                    <a href="#" title="Nombre de usuario comment"><img src="img/user4.jpg" alt="Imagen de perfil del usuario" class="img-circle img-user-mini"> User comments your post</a>
-                </li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#" title="All notifications">All Notifications</a></li>
-            </ul>
-        </div>
-        <div class="second-icon menu-icon">
-            <span><a href="perfil" title="Perfil"><span class="hidden-xs hidden-sm">Perfil</span> <i class="fa fa-user" aria-hidden="true"></i></a>
-            </span>
-        </div>
-        <div class="second-icon menu-icon">
-            <span><a href="muro" title="Wall"><span class="hidden-xs hidden-sm">Muro</span> <i class="fa fa-database" aria-hidden="true"></i></a>
-            </span>
+
         </div>
     </header>
+    <!--Header-->
+    <!-- <header class="text-right">
+        <div class="col-12 navbar navbar-expand-md bg-dark navbar-dark float-right ">
+            <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-right " id="menu">
+                <form class="text-left search" method="GET">
+                    <input name="q" type="text" placeholder="Buscar..">
+                </form>
+                <div class="  menu-icon">
+                    
 
+                    <div class="dropdown d-right ">
+                        <span class="dropdown-toggle" role="button" id="dropdownSettings" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <span class="">Configuracion</span> <i class="fa fa-cogs" ></i>
+                        </span>
+                        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownSettings">
+                            <li>
+                                <a href="#" title="Editar usuario" data-toggle="modal" data-target="#settingsmodal">
+                                    <div class="col-xs-4">
+                                        <i class="fa fa-wrench"></i>
+                                    </div>
+                                    <div class="col-xs-8 text-left">
+                                        <span>Editar usuario</span>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class=" col-3 second-icon dropdown menu-icon  ">
+                    <span class="col-2 dropdown-toggle float-right" role="button" id="dropdownNotification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <span class="">Notificaciones</span> <i class="fa fa-bell-o" aria-hidden="true"></i> <span class="badge">2</span>
+                    </span>
+                    <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownNotification">
+                        <li class="new-not">
+                            <a href="#" title="Nombre de usuario comment"><img src="img/Amigo2.jpg" alt="Imagen de perfil del usuario" class="img-circle img-user-mini"> User comments your post</a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#" title="All notifications">All Notifications</a></li>
+                    </ul>
+                </div>
+                
+                <div class="col-xs-12 col-md-3 col-lg-2 float-right ">
+                    <div class=' menu-icon'>
+                        <span class='col text-white'><a href="Perfil" title="Perfil"><span >Perfil</span><i class="fa fa-user"></i></a>
+                        </span>
+                    </div>  
+                </div>
+                <div class="col-xs-12 col-md-3 col-lg-2  float-right ">
+                    <div class=' menu-icon'>
+                        <span class='col'><a href="muro" title="Wall"><span >Muro</span> <i class="fa fa-database" ></i></a>
+                        </span>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </header>
+ -->
     <!--Barra de costado con el Perfil -->
-    <div class="sidebar-nav">
+    <div class=" sidebar-nav d-none d-lg-block " style=" position: fixed;position: fixed;
+    width: 240px;
+    top: 0px;
+    bottom:0px;
+    padding: 50px 10px 0px 10px;
+    background: #e1e8f0;
+    z-index: 99; width: 230px; height: inherit;">
         <a href="perfil" title="Perfil">
             <img alt="ImagenPerfil" class="img-circle img-user" src="/storage/{{Auth::user()->foto_perfil}}">
         </a>
-        <form action="/userPrueba" method='POST' enctype='multipart/form-data'>
+        <!-- <form action="/userPrueba" method='POST' enctype='multipart/form-data'>
             @csrf
             <input type='hidden' name='id' value='{{Auth::user()->id}}'>
             <input type="file" name='foto_perfil' '> Cambia tu foro de Perfil
             <button type='submit'>Cambiar</button>
-        </form>
+        </form> -->
         <h2 class="text-center hidden-xs"><a href="perfil-personal.php" title="Perfil"></a>{{Auth::user()->name }} {{Auth::user()->surname }}</h2>
         <p class="text-center user-description hidden-xs">
             <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</i>
@@ -124,7 +209,7 @@
         });
 
     </script>
-
+  </div>
 </body>
 
 
